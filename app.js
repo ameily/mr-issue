7f431acaf378fc4fc986df151ace9cb9b517236d
Adam Meily <meily.adam@gmail.com>
2015-06-08 16:27:40 -0400

    added config parsing and request handling

--- app.js
+++ app.js
@@ -1,32 +1,43 @@
 
 var http = require('http');
 var util = require('util');
+var MrIssueDriver = require('./driver');
+var MrIssueConfig = require('./config');
 
-http.createServer(function(req, res) {
-    // req.url == "/endpoint"
+var config = new MrIssueConfig();
+var driver = new MrIssueDriver(config, function(err) {
+  if(err) {
+    console.log("Could not initialize Mr. Issue driver: " + err);
+    return;
+  }
 
-    var data = "";
-    req.on('data', function(chunk) {
-        data += chunk;
-    });
+  http.createServer(function(req, res) {
+      // req.url == "/endpoint"
 
-    req.on('end', function() {
-        var body = null;
-        try {
-            body = JSON.parse(data);
-        } catch(e) {
-        }
-        
-        if(body) {
+      var data = "";
+      req.on('data', function(chunk) {
+          data += chunk;
+      });
 
-        }
+      req.on('end', function() {
+          var body = null;
+          try {
+              body = JSON.parse(data);
+          } catch(e) {
+          }
 
-        console.log(req.url);
-        console.log(util.inspect(body, false, null, true));
-        console.log("\n");
+          if(body) {
 
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end('{"ok": true}');
-    });
-}).listen(8080, "0.0.0.0");
+          }
 
+          console.log(req.url);
+          console.log(util.inspect(body, false, null, true));
+          console.log("\n");
+
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+          res.end('{"ok": true}');
+
+          driver.handleWebHook(req.url, body);
+      });
+  }).listen(8080, "0.0.0.0");
+});
