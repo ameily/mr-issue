6cef9228112dde3dec49c9d373eb9f8b3a8ab001
Adam Meily <meily.adam@gmail.com>
2015-06-09 00:05:26 -0400

    added colors to log

--- logger.js
+++ logger.js
@@ -1,7 +1,14 @@
 var winston = require('winston');
 var util = require('util');
 var moment = require('moment');
-//var colors = require('colors/safe');
+var colors = require('colors/safe');
+
+var colorMap = {
+  info: colors.green,
+  warn: colors.yellow,
+  error: colors.red,
+  debug: colors.cyan
+};
 
 var logger = new winston.Logger({
   transports: [
@@ -11,16 +18,19 @@ var logger = new winston.Logger({
       //timestamp: true,
       showLevel: true,
 
+
+
       timestamp: function() {
         return moment().format("HH:mm:ss");
       },
 
       formatter: function(opts) {
+        var c = colorMap[opts.level].bold || function(s) { return s; };
 
-
-        //var c = opt.level in colorMap ? colorMap(opts.level) : function(s) { return s; };
-
-        return util.format("[%s] %s %s", opts.timestamp(), opts.level, opts.message);
+        return util.format(
+          "[%s] %s %s", colors.grey.bold(opts.timestamp()),
+          c(opts.level.toUpperCase()), opts.message
+        );
       }
     })
   ]
