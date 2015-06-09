8976d212c33e5f3dafd85d7bac1ae075e16a7955
Adam Meily <meily.adam@gmail.com>
2015-06-09 12:34:26 -0400

    improve log format and add exception stack tracing

--- logger.js
+++ logger.js
@@ -1,7 +1,14 @@
+///
+/// @copyright 2015 Adam Meily <meily.adam@gmail.com>
+///
+
 var winston = require('winston');
 var util = require('util');
 var moment = require('moment');
 var colors = require('colors/safe');
+var fs = require('fs');
+var _ = require('underscore');
+
 
 var colorMap = {
   info: colors.green,
@@ -10,15 +17,28 @@ var colorMap = {
   debug: colors.cyan
 };
 
+function getLevelString(level) {
+  if(level == "info") {
+    return "INFO ";
+  } else if(level == "warn") {
+    return "WARN ";
+  }
+  return level.toUpperCase();
+}
+
+try {
+  var stats = fs.statSync("./log");
+} catch(e) {
+  fs.mkdirSync("./log");
+}
+
+
 var logger = new winston.Logger({
   transports: [
     new winston.transports.Console({
       level: "debug",
       colorize: true,
-      //timestamp: true,
       showLevel: true,
-
-
 
       timestamp: function() {
         return moment().format("HH:mm:ss");
@@ -27,13 +47,44 @@ var logger = new winston.Logger({
       formatter: function(opts) {
         var c = colorMap[opts.level].bold || function(s) { return s; };
 
-        return util.format(
+        var log = util.format(
           "[%s] %s %s", colors.grey.bold(opts.timestamp()),
-          c(opts.level.toUpperCase()), opts.message
+          c(getLevelString(opts.level)), opts.message
         );
-      }
+
+        if(_.isObject(opts.meta) && !_.isEmpty(opts.meta)) {
+          log += "\n" + util.inspect(opts.meta);
+        }
+
+        return log;
+      },
+
+      handleExceptions: true
+    }),
+
+    new winston.transports.DailyRotateFile({
+      filename: "./log/app.log",
+      level: "debug",
+      tailable: true,
+      json: true,
+
+      timestamp: function() {
+        return moment().format("HH:mm:ss");
+      },
+
+      /*
+      formatters: function(opts) {
+        return util.format(
+          "[%s] %s %s", opts.timestamp(),
+          getLevelString(opts.level), opts.message
+        );
+      },
+      */
+
+      handleExceptions: true
     })
   ]
 });
+
 
 module.exports = logger;
